a428d7dc67b08d643920b186fa51ac85be950b5c
terappy <terakei818@gmail.com>
2019-01-13 00:48:31 +0900

    first game creation step complite

--- src/server.js
+++ src/server.js
@@ -14,46 +14,219 @@ const FIELD_WIDTH = 1000
 const FIELD_HEIGHT = 1000
 
 /***
- * Playerクラス
+ * GameObjectクラス
  */
-class Player {
+class GameObject {
     constructor(obj = {}) {
         this.id = Math.floor(Math.random() * 1000000000)
-        this.width = 80
-        this.height = 80
-        this.x = Math.random() * (FIELD_WIDTH - this.width)
-        this.y = Math.random() * (FIELD_HEIGHT - this.height)
-        /** playerの向き */
-        this.angle = 0
-        /** 移動方向 */
-        this.movement = {}
+        this.width = obj.width
+        this.height = obj.height
+        this.x = obj.x
+        this.y = obj.y
+        /** オブジェクトの向き */
+        this.angle = obj.angle
     }
 
     /***
      * 移動メソッド
      */
     move(distance) {
+        const oldX = this.x
+        const oldY = this.y
+
         this.x += distance * Math.cos(this.angle)
         this.y += distance * Math.sin(this.angle)
+
+        // 衝突判定処理
+        let collision = false
+        // 画面外かどうか
+        if (this.x < 0 || this.x + this.width >= FIELD_WIDTH || this.y < 0 || this.y + this.height >= FIELD_HEIGHT) {
+            collision = true
+        }
+        // 壁と衝突したか
+        if (this.intersectWalls()) {
+            collision = true
+        }
+        // 衝突したらオブジェクトを動かさない
+        if (collision) {
+            this.x = oldX
+            this.y = oldY
+        }
+        return !collision
     }
+
+    /** オブジェクトとの衝突判定 */
+    intersect(obj) {
+        return (this.x <= obj.x + obj.width) &&
+            (this.x + this.width >= obj.x) &&
+            (this.y <= obj.y + obj.height) &&
+            (this.y + this.height >= obj.y)
+    }
+
+    /** 壁の衝突判定 */
+    intersectWalls() {
+        return Object.values(walls).some((wall) => this.intersect(wall))
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            x: this.x,
+            y: this.y,
+            width: this.width,
+            height: this.height,
+            angle: this.angle
+        }
+    }
+}
+
+/**
+ * Playerクラス
+ */
+class Player extends GameObject {
+    constructor(obj = {}) {
+        super(obj)
+        this.socketId = obj.socketId
+        this.nickname = obj.nickname
+        this.width = 80
+        this.height = 80
+        /** プレイヤーの体力 */
+        this.health = this.maxHealth = 10
+        this.bullets = {}
+        /** プレイヤーの得点 */
+        this.point = 0
+        this.movement = {}
+
+        // プレイヤーの初期位置設定（壁の中なら再設定）
+        do {
+            this.x = Math.random() * (FIELD_WIDTH - this.width)
+            this.y = Math.random() * (FIELD_HEIGHT - this.height)
+            this.angle = Math.random() * 2 * Math.PI
+        } while (this.intersectWalls())
+    }
+
+    shoot() {
+        // ステージ上で存在できる弾の上限は3発
+        if (Object.keys(this.bullets).length >= 3) {
+            return
+        }
+
+        // 弾の生成、位置をプレイヤーの真ん中に設定
+        const bullet = new Bullet({
+            x: this.x + this.width / 2,
+            y: this.y + this.height / 2,
+            angle: this.angle,
+            player: this
+        })
+
+        bullet.move(this.width / 2)
+        this.bullets[bullet.id] = bullet
+        bullets[bullet.id] = bullet
+    }
+
+    damage() {
+        this.health--
+        if (this.health === 0) {
+            this.remove()
+        }
+    }
+
+    remove() {
+        delete players[this.id]
+        io.to(this.socketId).emit('dead')
+    }
+
+    toJSON() {
+        return Object.assign(super.toJSON(), { health: this.health, maxHealth: this.maxHealth, socketId: this.socketId, point: this.point, nickname: this.nickname })
+    }
+}
+
+class Bullet extends GameObject {
+    constructor(obj) {
+        super(obj)
+        this.width = 15
+        this.height = 15
+        this.player = obj.player
+    }
+
+    remove() {
+        delete this.player.bullets[this.id]
+        delete bullets[this.id]
+    }
+}
+
+class BotPlayer extends Player {
+    constructor(obj) {
+        super(obj)
+        this.timer = setInterval(() => {
+            if (!this.move(4)) {
+                this.angle = Math.random() * Math.PI * 2
+            }
+            if (Math.random() < 0.3) {
+                this.shoot()
+            }
+        }, 1000 / 30)
+    }
+
+    remove() {
+        super.remove()
+        clearInterval(this.timer)
+        setTimeout(() => {
+            const bot = new BotPlayer({ nickname: this.nickname })
+            players[bot.id] = bot
+        }, 3000)
+    }
+}
+
+class Wall extends GameObject {
+
 }
 
 /** プレイヤー一覧 */
 let players = {}
+/** 弾一覧 */
+let bullets = {}
+/** 壁一覧 */
+let walls = {}
+
+// 壁を生成
+for (let i = 0; i < 3; i++) {
+    const wall = new Wall({
+        x: Math.random() * FIELD_WIDTH,
+        y: Math.random() * FIELD_HEIGHT,
+        width: 200,
+        height: 50
+    })
+    walls[wall.id] = wall
+}
+
+// bot生成
+const bot = new BotPlayer({ nickname: 'bot' })
+players[bot.id] = bot
 
 io.on('connection', (socket) => {
     let player = null
 
     // ゲーム開始時の処理
     socket.on('game-start', (config) => {
-        player = new Player()
+        player = new Player({
+            socketId: socket.id,
+            nickname: config.nickname
+        })
         players[player.id] = player
     })
 
     // プレイヤー移動処理
     socket.on('movement', (movement) => {
-        if (!player) { return }
+        if (!player || player.health === 0) { return }
         player.movement = movement
+    })
+
+    // 弾発射処理
+    socket.on('shoot', () => {
+        console.log('shoot')
+        if (!player || player.health === 0) { return }
+        player.shoot()
     })
 
     // 通信終了処理
@@ -65,6 +238,7 @@ io.on('connection', (socket) => {
 })
 
 setInterval(() => {
+    // 各プレイヤーごとの処理
     Object.values(players).forEach((player) => {
         const movement = player.movement
         if (movement.forward) {
@@ -80,10 +254,29 @@ setInterval(() => {
             player.angle += 0.1
         }
     })
-    io.sockets.emit('state', players)
+
+    // 弾ごとの処理
+    Object.values(bullets).forEach((bullet) => {
+        if (!bullet.move(10)) {
+            bullet.remove()
+            return
+        }
+        Object.values(players).forEach((player) => {
+            if (bullet.intersect(player)) {
+                if (player !== bullet.player) {
+                    player.damage()
+                    bullet.remove()
+                    bullet.player.point += 1
+                }
+            }
+        })
+    })
+
+    // クライアントにオブジェクトの状態を送信
+    io.sockets.emit('state', players, bullets, walls)
 }, 1000 / 30)
 
-app.use('/static', express.static(path.join(__dirname, '/static')))
+app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static/index.html'))
--- src/static/game.js
+++ src/static/game.js
@@ -4,17 +4,20 @@
 const socket = io()
 /** canvasオブジェクトをHTML要素から取得 */
 const canvas = $('#canvas-2d')[0]
-/** 描画コンテキストを取得 */
+/** 描画コンテキスト */
 const context = canvas.getContext('2d')
 const playerImage = $('#player-image')[0]
 
-/** playerの動き保存用変数 */
-let movement = {}
-
 /** ゲーム開始時の処理 */
 function gameStart() {
-    socket.emit('game-start')
+    socket.emit('game-start', { nickname: $('#nickname').val() })
+    $('#start-screen').hide()
 }
+
+$('#start-button').on('click', gameStart)
+
+/** playerの動き保存用変数 */
+let movement = {}
 
 $(document).on('keydown keyup', (event) => {
     const KeyToCommand = {
@@ -32,6 +35,9 @@ $(document).on('keydown keyup', (event) => {
         }
         socket.emit('movement', movement)
     }
+    if (event.key === ' ' && event.type === 'keydown') {
+        socket.emit('shoot')
+    }
 })
 
 /** playerの状態を受け取り画面に描写する */
@@ -44,10 +50,40 @@ socket.on('state', (players, bullets, walls) => {
     context.stroke()
 
     Object.values(players).forEach((player) => {
-        context.drawImage(playerImage, player.x, player.y)
-        context.font = '30px Bold Arial'
-        context.fillText('Player', player.x, player.y - 20)
+        context.save()
+        context.font = '20px Bold Arial'
+        context.fillText(player.nickname, player.x, player.y + player.height + 25)
+        context.font = '10px Bold Arial'
+        context.fillStyle = 'gray'
+        context.fillText('🖤'.repeat(player.maxHealth), player.x, player.y + player.height + 10)
+        context.fillStyle = 'red'
+        context.fillText('❤️'.repeat(player.health), player.x, player.y + player.height + 10)
+        context.translate(player.x + player.width / 2, player.y + player.height / 2)
+        context.rotate(player.angle)
+        context.drawImage(playerImage, 0, 0, playerImage.width, playerImage.height, -player.width / 2, -player.height / 2, player.width, player.height)
+        context.restore()
+
+        if (player.socketId === socket.id) {
+            context.save()
+            context.font = '30px Bold Arial'
+            context.fillText('You', player.x, player.y - 20)
+            context.fillText(player.point + ' point', 20, 40)
+            context.restore()
+        }
+    })
+
+    Object.values(bullets).forEach((bullet) => {
+        context.beginPath()
+        context.arc(bullet.x, bullet.y, bullet.width / 2, 0, 2 * Math.PI)
+        context.stroke()
+    })
+
+    Object.values(walls).forEach((wall) => {
+        context.fillStyle = 'black'
+        context.fillRect(wall.x, wall.y, wall.width, wall.height)
     })
 })
 
-socket.on('connect', gameStart)
+socket.on('dead', () => {
+    $('#start-screen').show()
+})
